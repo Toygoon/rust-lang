cc2eca795569a8686c504ec8b2b490b47073adc6
Toygoon <toygoon@gmail.com>
2023-09-13 23:24:14 +0900

    P030, 타입 지정

--- Chap02/project2-1/src/main.rs
+++ Chap02/project2-1/src/main.rs
@@ -1,12 +1,13 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("숫자를 맞혀봅시다!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("사용자가 맞혀야 할 숫자 : {}", secret_number);
-    
+
     println!("정답이라고 생각하는 숫자를 입력하세요.");
 
     let mut guess = String::new();
@@ -14,5 +15,15 @@ fn main() {
         .read_line(&mut guess)
         .expect("입력한 값을 읽지 못했습니다.");
 
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("입력한 값이 올바른 숫자가 아닙니다.");
+
     println!("입력한 값 : {}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("입력한 숫자가 작습니다!"),
+        Ordering::Greater => println!("입력한 숫자가 큽니다!"),
+        Ordering::Equal => println!("정답!"),
+    }
 }
