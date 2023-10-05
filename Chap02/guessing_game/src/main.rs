fc4b9bed68a21f0e947c4fb2e4bd3f25d6d9c40f
Toygoon <toygoon@gmail.com>
2023-10-05 21:43:50 +0900

    P037, Chap02 완료

--- Chap02/guessing_game/src/main.rs
+++ Chap02/guessing_game/src/main.rs
@@ -4,13 +4,9 @@ use std::io;
 
 fn main() {
     println!("숫자를 맞혀봅시다!");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("사용자가 맞혀야 할 숫자 : {}", secret_number);
-
+    
     loop {
-        println!("정답이라고 생각하는 숫자를 입력하세요.");
-
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -21,7 +17,6 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!("입력한 값 : {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("입력한 숫자가 작습니다!"),
             Ordering::Greater => println!("입력한 숫자가 큽니다!"),
