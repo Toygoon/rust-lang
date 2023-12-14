1376c5a5852e52ca46e479c8eb6062ea5fe5a00e
Toygoon <toygoon@gmail.com>
2023-12-14 22:24:35 +0900

    P113, 메서드 정의하기?

--- Chap05/rectangles/src/main.rs
+++ Chap05/rectangles/src/main.rs
@@ -1,6 +1,13 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
