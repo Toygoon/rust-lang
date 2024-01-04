9825b0049f95f9e120ec4652b4cb8ab30b6c235c
Toygoon <toygoon@gmail.com>
2024-01-04 22:40:05 +0900

    P143, 예제 7-3

---
+++ Chap07/restaurant/src/lib.rs
@@ -0,0 +1,35 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hostring::add_to_waitlist();
+
+    // relative path
+    front_of_house::hosting::add_to_waitlist();
+}
